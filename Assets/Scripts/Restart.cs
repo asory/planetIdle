c60a97bf183d62843bbd8ff9de12ff0ea0276ae5
Asory <rosablanca.pg@gmail.com>
2019-08-27 00:45:55 -0400

    add Restart and screen capture

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Restart : MonoBehaviour
+{
+    public Button finish;
+    void Start()
+    {
+        finish.onClick.AddListener(RestartScene);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+
+}
